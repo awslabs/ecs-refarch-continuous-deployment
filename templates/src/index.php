c609253edaa058fc4e87076384c6be2a853ffe91
deepak-tiwari-comprinno <deepak.tiwari@comprinno.net>
2022-11-22 11:32:02 +0530

    commiting index.php file

---
+++ templates/src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
+?>
+
+</body>
+</html>
